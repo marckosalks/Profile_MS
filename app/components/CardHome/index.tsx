bea0e2294d11313c87d7c2de94d9131c69149c9b
marckosalks <m4rcos.sales@gmail.com>
2025-04-11 11:53:55 -0300

    feat: ajustando line-height

--- app/components/CardHome/index.tsx
+++ app/components/CardHome/index.tsx
@@ -5,12 +5,12 @@ export default function CardHome() {
   return (
     <div className="h-screen w-full flex animate-gradient bg-gradient-to-b from-blue-400 via-blue-600 to-blue-950 bg-[length:200%_200%]">
 
-    <section className="h-screen w-full flex items-center justify-center">
+    <section className="h-screen w-full flex items-center justify-center ">
       <div className=" text-white uppercase ">
         <h1 className="text-[1.5rem] font-bold block ">Olá!<br />
-          <span className="text-[5rem] ">Me chame de <br />Tigger</span>
-          <span className="text-2xl font-normal normal-case"><br />Seu desenvolvedor compentente.</span>
+          <span className="text-[5rem] leading-none">Me chame de <br />Tigger</span>
         </h1>
+        <span className="text-2xl font-normal normal-case leading-none"><br />Seu desenvolvedor compentente.</span>
       </div>
 
       <Image src={avatar} alt="avatar Marcos Sales" className='w-lg scale-150 pt-[74]' />
